5a14d3c1112c0f939f088ae1c720a3b66977ebbb
AndyCotruta <acotruta18@gmail.com>
2022-10-04 14:35:19 +0200

    1st part of today's homework

---
+++ index.js
@@ -0,0 +1,9 @@
+let firstName = "Andy";
+let middleName = "Aristide";
+let lastName = "Cotruta";
+let fullName = firstName + " " + middleName + " " + lastName;
+console.log("hello, my name is " + fullName);
+let myAge = 28;
+let currentYear = 2022;
+let birthYear = currentYear - myAge;
+console.log("my birth year is " + birthYear);
